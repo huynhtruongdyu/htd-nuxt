35adda16b67a6a8d11018a9c1cbbc50399c21e1e
Duy Huynh <huynhtruongdyu@gmail.com>
2024-04-15 13:06:04 +0700

    init

---
+++ components/AppHeader.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { black } from "../assets/styles/scss/color.module.scss";
+console.log("colors", black);
+</script>
+<template>
+  <header>Header</header>
+</template>
+
+<style lang="scss"></style>
